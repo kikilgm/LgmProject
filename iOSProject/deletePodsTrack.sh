fe11c26bbaa41c74aa76ea87ce568ea5bcbaf73f
liguangming <kikilgm@qq.com>
2020-11-04 14:40:07 +0800

    11

--- iOSProject/deletePodsTrack.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-git rm --cached Pods/Headers/\* --ignore-unmatch
-git rm --cached Pods/Target\ Support\ Files/\* --ignore-unmatch
-git rm --cached Pods/Local\ Podspecs/\* --ignore-unmatch
-git rm --cached Pods/Pods.xcodeproj/\* --ignore-unmatch
-git rm --cached Pods/\* --ignore-unmatch
-git commit -m "delete pods files"
